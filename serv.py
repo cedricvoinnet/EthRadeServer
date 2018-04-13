498b1ae99903645d4375eb2536286e2c3e8460e4
voinne_c <cedric.voinnet@epitech.eu>
2018-04-13 16:29:44 +0200

    add route for history, users and visibility changes

--- serv.py
+++ serv.py
@@ -16,16 +16,19 @@ def root():
 def register():
     new_user = {
         'username' : request.json.get('username'),
-        'password' : request.json.get('password')
+        'password' : request.json.get('password'),
+        'key' : request.json.get('key'),
+        'contacts' : [],
+        'visible' : True
     }
-    if new_user['username'] is None or new_user['password'] is None:
+    if new_user['username'] is None or new_user['password'] is None or new_user['key'] is None:
         return make_response(jsonify('missing parameters'), 400)
-    u = db.users.find_one({'username': new_user['username']})
+    u = db.users.find_one({'key': new_user['key']})
     if u is None:
         db.users.insert_one(new_user)
     else:
         return make_response(jsonify('user already exists'), 403)
-    return make_response(jsonify({'token': 'token'}), 200)
+    return make_response(jsonify({'key': new_user['key']}), 200)
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -40,7 +43,7 @@ def login():
         return make_response(jsonify('user does not exists'), 403)
     if u['password'] != user['password']:
         return make_response(jsonify('not authentified'), 401)
-    return make_response(jsonify({'token': 'token'}), 200)
+    return make_response(jsonify({'key': new_user['key']}), 200)
 
 @app.route('/contacts', methods=['GET', 'POST'])
 def contacts():
@@ -63,7 +66,54 @@ def contacts():
         return make_response(jsonify('successfully added'), 200)
     return make_response(jsonify(dumps(u['contacts'])), 200)
 
-@app.route('/contact/<username>', methods=['GET'])
-def contact(username):
-    ret = db.users.find({'username': username})
-    return make_response(jsonify(ret), 200)
+@app.route('/users', methods=['GET'])
+def users():
+    user = {
+        'username' : request.headers.get('username'),
+        'password' : request.headers.get('password')
+    }
+    u = db.users.find_one({'username': user['username']})
+    if u is None:
+        return make_response(jsonify('user does not exists'), 403)
+    if u['password'] != user['password']:
+        return make_response(jsonify('not authentified'), 401)
+    ret = db.users.find({}, {'_id': 0, 'password': 0, 'contacts': 0})
+    return make_response(jsonify(dumps(ret)), 200)
+
+@app.route('/visible', methods=['POST'])
+def visible():
+    user = {
+        'username' : request.headers.get('username'),
+        'password' : request.headers.get('password')
+    }
+    u = db.users.find_one({'username': user['username']})
+    if u is None:
+        return make_response(jsonify('user does not exists'), 403)
+    if u['password'] != user['password']:
+        return make_response(jsonify('not authentified'), 401)
+    res = db.users.update_one({'username': user['username']}, {'$set': {'visible':request.get.json('visible')}})
+    return make_response(jsonify('successfully changed'), 200)
+
+@app.route('/history', methods=['GET', 'POST'])
+def history():
+    user = {
+        'username' : request.headers.get('username'),
+        'password' : request.headers.get('password')
+    }
+    u = db.users.find_one({'username': user['username']})
+    if u is None:
+        return make_response(jsonify('user does not exists'), 403)
+    if u['password'] != user['password']:
+        return make_response(jsonify('not authentified'), 401)
+
+    if request.method == 'POST':
+        new_history = {
+            'to': request.json.get('to'),
+            'from': request.json.get('from'),
+            'amount': request.json.get('amount')
+        }
+        db.histories.insert_one(new_history)
+        return make_response(jsonify('successfully added'), 200)
+
+    ret = db.histories.find({'$or': [{'to': u['key']}, {'from': u['key']}]}, {'_id': 0})
+    return make_response(jsonify(dumps(ret)), 200)
